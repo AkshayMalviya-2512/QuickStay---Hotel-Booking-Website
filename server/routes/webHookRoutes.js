ff1a32839fd3eda0a287d8fea0d573404c0bd40e
AkshayMalviya-2512 <akshaymalviya025@gmail.com>
2026-04-09 21:28:48 +0530

    Delete server directory

--- server/routes/webHookRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import clerkWebHooks from "../controllers/clerkWebHooks.js";
-
-const router = express.Router();
-
-router.post("/clerk", clerkWebHooks);
-
-export default router;
